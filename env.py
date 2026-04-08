8276cce310a39e5d562675c51ea26015218c3fdc
adity727758 <shhhd1802@gmail.com>
2026-04-08 23:27:19 +0530

    Update bot token, admin user ID, and required channels

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
-BOT_TOKEN=8742926503:AAErzY1QBfW5cuugP95SMdrCcFayR00cMg0
+BOT_TOKEN=8760406918:AAFizK1u-4uKMQfz0OrgIG9OKZxLhVuNWz0
 MONGODB_URI=mongodb+srv://rajkhilchi786:F1uEyWGo9UTeqmTV@cluster0.bug4pqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 DATABASE_NAME=rajkhilchi786
 API_URL=https://app.battle-destroyer.shop
 API_KEY=ak_acab7fd1d7ce18e568801e8f6bf82f672a0da0639489c88e
-ADMIN_USER_ID=8459937381
-REQUIRED_CHANNELS=@ItsMeVishalBots
+ADMIN_USER_ID=8487946379
+REQUIRED_CHANNELS=@HanmaBaby
